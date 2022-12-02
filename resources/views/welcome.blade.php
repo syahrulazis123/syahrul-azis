5277c5abc789389907f5dfadecda63e0326fe171
syahrul <syahrulazis0706@.com>
2022-12-02 15:43:20 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<meta http-equiv="content-security0policy"content="upgrade-request">
+    <link rel="stylesheet" href="{{asset('stylescv.css')}}">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="{{asset('foto.jpeg')}}" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">Syahrul Azis</p>
+           <p class="regular">Web Designer | Youtuber</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Aceh <br /> Indonesia
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +62822946704382
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               syahrulazis0706<br />@gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               HTML
+             </div>
+             <div class="skill_progress">
+               <span style="width: 80%;"></span>
+             </div>
+             <div class="skill_per">80%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               CSS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 70%;"></span>
+             </div>
+             <div class="skill_per">70%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Corel
+             </div>
+             <div class="skill_progress">
+               <span style="width: 90%;"></span>
+             </div>
+             <div class="skill_per">90%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               JS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 60%;"></span>
+             </div>
+             <div class="skill_per">60%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               PHP
+             </div>
+             <div class="skill_progress">
+               <span style="width: 88%;"></span>
+             </div>
+             <div class="skill_per">88%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 80%;"></span>
+                  </div>
+                  <div class="skill_per">80%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 70%;"></span>
+                </div>
+                <div class="skill_per">70%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+                  Melayu
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 70%;"></span>
+                </div>
+                <div class="skill_per">70%</div>
+              </li>
+          </li>
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is Syahrul Azis, I'm 21 Years Old, I From Aceh, meulaboh Island, Indonesia</p>
+        <p>I Graduated from Uin Ar raniry University From Major Computer Engineering</p>
+        <p>My Hoby is Playing Games, Watching movie, and Eat</p>
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+                <div class="date">2003-2009</div> 
+                <div class="info">
+                     <p class="semi-bold">SDN 1 meulaboh</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2009-2012</div>
+              <div class="info">
+                     <p class="semi-bold">smp 1 meulaboh</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2012 - 2015</div>
+              <div class="info">
+                     <p class="semi-bold">MAN 1 Banda Aceh</p> 
+                  <p>Major: Computer And Network Engineering</p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2015 - 2020</div>
+              <div class="info">
+                     <p class="semi-bold"> Ar raniry University, Aceh</p> 
+                  <p>Major: Bachelor Computer Engineering</p>
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Work Experience</p>
+         </div>
+      <ul>
+            <li>
+                <div class="date">2015 - 2015</div> 
+                <div class="info">
+                     <p class="semi-bold">Work At Copy.com Banda Aceh as Computer Repair Worker</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2018 - 2018</div>
+              <div class="info">
+                <p class="semi-bold">Internship At Witel Telkom Aceh as Web Designer</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2019 - 2019</div>
+              <div class="info">
+                  <p class="semi-bold">Internship At Telkom Prima Cipta Certifia At Aceh as Web Designer</p> 
+                  
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-book"></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class="fas fa-music"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
